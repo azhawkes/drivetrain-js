74dd0d8f0036035f3f57bd938ee75bfbae2f568a
Andy Hawkes <andy@andyhawkes.com>
2015-02-17 12:14:02 -0700

    New HTTP verbs.

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ module.exports = {
         return http.post(drivetrain.configuration, url);
     },
 
+    put: function (url) {
+        return http.put(drivetrain.configuration, url);
+    },
+
+    delete: function (url) {
+        return http.delete(drivetrain.configuration, url);
+    },
+
+    options: function (url) {
+        return http.options(drivetrain.configuration, url);
+    },
+
+    trace: function (url) {
+        return http.trace(drivetrain.configuration, url);
+    },
+
+    method: function (method, url) {
+        return http.method(drivetrain.configuration, method, url);
+    },
+
     wait: function (ms) {
         return wait.wait(drivetrain.configuration, ms);
     }
