55479a6b3906b19f7d64a080f5679fb3b9f0b155
Andy Hawkes <andy@andyhawkes.com>
2015-02-17 12:13:39 -0700

    Updated example for new body inspections.

--- examples/example.js
+++ examples/example.js
@@ -5,22 +5,22 @@ drivetrain.configure({
 });
 
 drivetrain.run([
+    drivetrain.get('http://localhost:4000/hello'),
+
     drivetrain.get('http://www.loadsterperformance.com').withResources([
         '/main.css',
         '/main.js',
         '/favicon.ico'
     ]).validateBodyContains("Loadster").validateHeaderContains("Content-Type", "html").validateBodyAsString(function (body) {
         return body.length > 200 && body.indexOf("Booyah") >= 0;
-    }).scan(function (context, headers, body) {
+    }).inspect(function (context, headers, body) {
     }),
 
     drivetrain.wait(3000),
 
-    drivetrain.post('http://www.loadsterperformance.com/login').withBody({
+    drivetrain.post('http://www.loadsterperformance.com/login').withFormBody({
         j_username: "andy@andyhawkes.com",
         j_password: "asdf12345"
-    }).withHeaders({
-        "Content-Type": "application/x-www-form-urlencoded"
     })
 ], function () {
     console.log("Done!");
