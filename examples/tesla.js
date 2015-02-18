d64f222d63ebd71a060b3dafe64141e566cbcbb8
Andy Hawkes <andy@andyhawkes.com>
2015-02-17 18:03:20 -0700

    Cleaned up the Tesla example.

--- examples/tesla.js
+++ examples/tesla.js
@@ -1,5 +1,6 @@
 var drivetrain = require("../drivetrain");
-var vehicleId = "";
+var teslaUsername = "rich.guy@teslamotors.com";
+var teslaPassword = "iamrich";
 
 drivetrain.configure({
 //    baseUrl: "https://portal.vn.teslamotors.com",
@@ -7,27 +8,25 @@ drivetrain.configure({
     acceptCookies: true
 });
 
+var vehicleId = ""; // we'll parse this from a response
+
 drivetrain.run([
     drivetrain.get("/login"),
 
     drivetrain.post("/login").withFormBody({
-        "user_session[email]": "andy@andyhawkes.com",
-        "user_session[password]": "iforgot"
+        "user_session[email]": teslaUsername,
+        "user_session[password]": teslaPassword
     }).validateStatus(302).validateBodyContains("Welcome"),
 
     drivetrain.wait(3000),
 
     drivetrain.get("/vehicles").inspect(function (context, headers, body) {
-        var json = JSON.parse(body);
-
-        vehicleId = json[0].vehicle_id;
+        vehicleId = JSON.parse(body)[0].vehicle_id; // extract our vehicle_id
     }).then(function() {
         drivetrain.get("/vehicles/" + vehicleId + "/command/honk_horn").validateBodyAsJson(function (body) {
-            var honked = body.result;
+            console.log("horn honked? " + body.result);
 
-            console.log("horn honked? " + honked);
-
-            return honked;
+            return body.result;
         }).run();
     })
 ]);
