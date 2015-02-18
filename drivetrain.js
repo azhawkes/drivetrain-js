b73dfdf0a921c678f95cf49eab942c2bb335ad61
Andy Hawkes <andy@andyhawkes.com>
2015-02-17 17:41:00 -0700

    Better chaining and cookie support.

--- drivetrain.js
+++ drivetrain.js
@@ -10,34 +10,34 @@ module.exports = {
     runParallel: drivetrain.runParallel,
 
     get: function (url) {
-        return http.get(drivetrain.configuration, url);
+        return http.get(drivetrain.context, url);
     },
 
     post: function (url) {
-        return http.post(drivetrain.configuration, url);
+        return http.post(drivetrain.context, url);
     },
 
     put: function (url) {
-        return http.put(drivetrain.configuration, url);
+        return http.put(drivetrain.context, url);
     },
 
     delete: function (url) {
-        return http.delete(drivetrain.configuration, url);
+        return http.delete(drivetrain.context, url);
     },
 
     options: function (url) {
-        return http.options(drivetrain.configuration, url);
+        return http.options(drivetrain.context, url);
     },
 
     trace: function (url) {
-        return http.trace(drivetrain.configuration, url);
+        return http.trace(drivetrain.context, url);
     },
 
     method: function (method, url) {
-        return http.method(drivetrain.configuration, method, url);
+        return http.method(drivetrain.context, method, url);
     },
 
     wait: function (ms) {
-        return wait.wait(drivetrain.configuration, ms);
+        return wait.wait(drivetrain.context, ms);
     }
 };
